633e37e0674a6b5c79b1b33797a7554b3f6c6e0e
Swetank Mohanty <swetank.mohanty@outlook.com>
2020-12-24 17:38:57 +0530

    Removed Angular application 'ngapp'

--- ngapp/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  profileUrl: 'https://gist.githubusercontent.com/shortthirdman/0f3985d62a79a56cb14285f0b17ea963/raw/',
-  googleMapsApiKey: ''
-};
