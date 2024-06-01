5466bbef67501813d525f09c4685473120f74636
Swetank Mohanty <swetank.mohanty@outlook.com>
2024-06-01 22:02:53 +0530

    Added publish script

---
+++ publish.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+
+ghpages.publish('site', {
+  branch: 'gh-pages',
+  add: true,
+  // dest: 'static/project',
+  // cname: 'shortthirdman.surge.sh',
+  user: {
+    name: 'Swetank Mohanty',
+    email: 'swetank.mohanty@outlook.com'
+  },
+  silent: true,
+  repo: 'https://github.com/shortthirdman/shortthirdman.git'
+}, (err) => {
+	console.error("Error while publishing to GitHub Pages", err);
+});
